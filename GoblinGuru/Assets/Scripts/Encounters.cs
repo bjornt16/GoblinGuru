4ae8db23830a3a17af5fb5aa8a0c79f4d40647cd
Frikki898 <frikki898@gmail.com>
2017-12-04 23:23:46 +0000

    added some options to example encounter

--- GoblinGuru/Assets/Scripts/Encounters.cs
+++ GoblinGuru/Assets/Scripts/Encounters.cs
@@ -22,14 +22,22 @@ public class Encounters : MonoBehaviour {
         Encounter e = new Encounter("Hungry grizzly bear");
 
 
-        State s1 = new State("You notice a large grizzly bear strolling towards you", false);
-        State s2 = new State("You are able to run away from the grizzly bear", true);
-        State s3 = new State("The grizzly bear is able catches up to you and slashes you with its claws", true);
+        State s0 = new State("You notice a large grizzly bear strolling towards you", false);
+        State s1 = new State("You are able to run away from the grizzly bear", true);
+        State s2 = new State("The grizzly bear is able catches up to you and mauls you with its paw", false);
+        State s3 = new State("You attack the grizzly with great ferocity visibly hurting the animal", true);
+        State s4 = new State("You attempt to strike the grizzly but the beast bites your hand as you are about to land the blow", false);
+        e.AddState(s0);
         e.AddState(s1);
         e.AddState(s2);
         e.AddState(s3);
-        EncounterChoice c = new EncounterChoice("Try to flee from the grizzly", "dex", 0.6, s2, s3);
-        e.states[0].AddChoice(c);
+        e.AddState(s4);
+        //e.AddState(s5);
+        EncounterChoice c1 = new EncounterChoice("Try to flee from the grizzly", "dex", 0.0, e.states[1], e.states[2]);
+        EncounterChoice c2 = new EncounterChoice("Stand your ground and attempt to attack the grizzly", "str", 1.0, e.states[3], e.states[4]);
+        e.states[0].AddChoice(c1);
+        e.states[0].AddChoice(c2);
+        e.states[2].AddChoice(c2);
 
         e.PlayEncounter();
     }
