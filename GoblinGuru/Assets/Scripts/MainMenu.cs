69bcc0632cd11ec23d6fda431a1a11d018a5f78b
bjornt16 <bjornt16@ru.is>
2017-12-05 23:39:40 +0000

    Added MainMenu standalone scene

--- GoblinGuru/Assets/Scripts/MainMenu.cs
+++ GoblinGuru/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+
+    public GameObject loadingScreen;
+    public GameObject mainMenuWrapper;
+    public UnityEngine.UI.Slider slider;
+    public UnityEngine.UI.Text percentageText;
+
+    public void NewGame()
+    {
+        mainMenuWrapper.SetActive(false);
+        StartCoroutine(LoadScene("Bjossi"));
+    }
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Bjossi");
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            percentageText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
 
     public void ExitGame()
     {
