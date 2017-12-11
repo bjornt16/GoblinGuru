a467ec3dc817f70d8f6702ed6190408b19b4ba53
bjornt16 <bjornt16@ru.is>
2017-12-11 14:27:12 +0000

    start of node/graph refinement

---
+++ GoblinGuru/Assets/xNode/Example/EncounterNodeGraph.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System;
+using XNode;
+
+/// <summary> Defines an example nodegraph. </summary>
+[Serializable, CreateAssetMenu(fileName = "EncounterGraph", menuName = "Node Graph/Encounter")]
+public class EncounterNodeGraph : NodeGraph {
+}
