d87ab64dcd244f1da1a02bfc9f5c41a0298f728f
Frikki898 <frikki898@gmail.com>
2017-12-12 11:06:05 +0000

    zooming for minimap

---
+++ GoblinGuru/Assets/Scripts/MinimapManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapManager : MonoBehaviour
+{
+
+    public Transform player;
+
+    void LateUpdate()
+    {
+        Vector3 newPos = player.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+    }
+
+    public void ZoomIn()
+    {
+        if (GetComponent<Camera>().orthographicSize > 4)
+        {
+            GetComponent<Camera>().orthographicSize -= 2;
+        }
+    }
+
+    public void ZoomOut()
+    {
+
+        if (GetComponent<Camera>().orthographicSize < 22)
+        {
+            GetComponent<Camera>().orthographicSize += 2;
+        }
+    }
+
+}
