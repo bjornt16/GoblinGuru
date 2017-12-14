8aee6f7ba137f31949dc64bbd49d8726ffc6a9fa
bjornt16 <bjornt16@ru.is>
2017-12-14 22:52:35 +0000

    changed main scene a little, text to textmesh + panel scale

--- GoblinGuru/Assets/Scripts/GameTurnUI.cs
+++ GoblinGuru/Assets/Scripts/GameTurnUI.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameTurnUI : MonoBehaviour {
 
-    public UnityEngine.UI.Text TurnNumber;
+    public TextMeshProUGUI TurnNumber;
     public UnityEngine.UI.Button RestButton;
     public UnityEngine.UI.Button EncounterButton;
 
